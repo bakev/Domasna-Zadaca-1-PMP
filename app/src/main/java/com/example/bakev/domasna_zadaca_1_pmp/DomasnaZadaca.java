490df24bc660fe46b4ea9f449d07e98f2e50d9dc
bakev <bakevvlatko@yahoo.com>
2017-02-25 12:30:50 +0100

    Cela domasna vo eden commit problem so github.exe file vo android studio

---
+++ app/src/main/java/com/example/bakev/domasna_zadaca_1_pmp/DomasnaZadaca.java
@@ -0,0 +1,13 @@
+package com.example.bakev.domasna_zadaca_1_pmp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DomasnaZadaca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_domasna_zadaca);
+    }
+}
